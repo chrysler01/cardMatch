af7480d00917d82db2eaa8c87f7e9d68c14bc9e2
chrysler01 <57878622+chrysler01@users.noreply.github.com>
2020-06-29 23:21:26 +0300

    Add files via upload

---
+++ src/components/About/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './About.scss';
+
+const About = () => {
+    
+
+    return(
+        <div className="about-wrapper">
+            <header className="about-header">
+                <h1>Card matching game</h1>
+            </header>
+            <section className="about-content">
+                <p>Made by Evgeni Kolodinski</p>
+            </section>
+        </div>
+    );
+};
+
+export default About;
